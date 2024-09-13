4ee383568be5f03b8c767b382f129af8902a4c4d
temitopeohassan <temitope.hassan.2@gmail.com>
2024-09-13 10:06:53 +0100

    frame files

---
+++ src/app/page.tsx
@@ -0,0 +1,43 @@
+import { getFrameMetadata } from '@coinbase/onchainkit/frame';
+import type { Metadata } from 'next';
+import { NEXT_PUBLIC_URL } from './config';
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: 'Play Now',
+      action: 'link', // Define the action as a link
+      target: 'https://simon-game-liart-psi.vercel.app/', // URL of the Simon Game
+    },
+  ],
+  image: `${NEXT_PUBLIC_URL}/ok.PNG`,
+  post_url: `${NEXT_PUBLIC_URL}/api/frame`,
+});
+
+export const metadata: Metadata = {
+  title: 'Simon Game',
+  description: 'HTML and Javascript Simon Memory game',
+  openGraph: {
+    title: 'Simon Game',
+    description: 'HTML and Javascript Simon Memory game',
+    images: [`${NEXT_PUBLIC_URL}/ok.PNG`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
+  const handlePlayNowClick = () => {
+    // Opens the Simon game in a new tab
+    window.open('https://simon-game-liart-psi.vercel.app/', '_blank');
+  };
+
+  return (
+    <div>
+      <h1>HTML and Javascript Simon Memory Game</h1>
+      <p>This page serves the initial frame metadata for the HTML and Javascript Simon Memory game Frame.</p>
+     
+    </div>
+  );
+}
